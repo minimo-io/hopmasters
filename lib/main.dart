7ca5cc136194e511d2f3d12e1cce4ce78be48b15
luciaerramuspe <lucia@erramuspe.com>
2020-12-14 14:53:13 -0300

    Home Page title: modificado 2

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hopmasters',
+      title: 'Hopmasters Beta',
       theme: ThemeData(
         // This is the theme of your application.
         //
