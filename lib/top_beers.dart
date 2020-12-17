b37052782977ac59655b9e5dd3537da367611371
minimo-io <nicolas@minimo.io>
2020-12-17 17:03:55 -0300

    Create a separate window for Saved Beers and list View.

--- lib/top_beers.dart
+++ lib/top_beers.dart
@@ -10,6 +10,7 @@ class TopBeers extends StatefulWidget{
 class TopBeersState extends State<TopBeers>{
 
   final _randomWordPairs = <WordPair>[];
+  final _savedBeers = Set<WordPair>();
 
   Widget _buildList(){
     return ListView.builder(
@@ -26,12 +27,59 @@ class TopBeersState extends State<TopBeers>{
   }
 
   Widget _buildRow(WordPair pair){
-    return ListTile(title: Text(pair.asPascalCase, style: TextStyle(fontSize:18.0)));
+    final alreadySaved = _savedBeers.contains(pair);
+    return ListTile(
+        title: Text(pair.asPascalCase, style: TextStyle(fontSize:18.0)),
+        trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border, color: alreadySaved ? Colors.red : null),
+        onTap: (){
+           setState(() {
+             if (alreadySaved){
+               _savedBeers.remove(pair);
+             }else{
+               _savedBeers.add(pair);
+             }
+           });
+        }
+    );
+  }
+  // actual navigator class & routing usage
+  // https://api.flutter.dev/flutter/widgets/Navigator-class.html
+  void _pushSaved(){
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (BuildContext context){
+          final Iterable<ListTile> tiles = _savedBeers.map((WordPair pair){
+            return ListTile(
+              title: Text(pair.asPascalCase, style: TextStyle(fontSize:16.0))
+            );
+          });
+          final List<Widget> divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text("Saved Beers")
+            ),
+            body: ListView(children: divided)
+          );
+        }
+      )
+    );
   }
 
   Widget build(BuildContext context){
     return Scaffold(
-        appBar: AppBar( title: Text( 'Top Beers' ) ),
+        appBar: AppBar(
+            title: Text( 'Hopmasters' ),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.list),
+                onPressed: _pushSaved
+              )
+            ]
+        ),
         body: _buildList()
 
     );
